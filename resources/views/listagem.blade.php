107a094568395f6cb1928efd63c1f8f8f0ac876a
Anderson Lairton <lairton.anderson@gmail.com>
2020-05-30 14:34:33 -0300

    rotas para edição e criação da pagina de listagem

---
+++ resources/views/listagem.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+
+@if(empty($posts))
+<div class="alert alert-danger">
+    Você ainda nao tem posts cadastrados
+</div>
+@else
+<div class="container">
+    <div class="card">
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand">Meus Posts</a>
+            <a href="{{route('posts.novo')}}" class="btn btn-info my-2 my-sm-0">Novo Post</a>
+        </nav>
+        <div class="card-body">
+            <div class="card-deck">
+                @foreach($posts as $p)
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$p->titulo}}</h5>
+                            <img class="card-img-top" src="{{$p->url_imagem}}" width="220" height="220" alt="Imagem de capa do card">
+                            <p class="card-text">{{$p->descricao}}</p>
+                            <a href="{{action('PostsController@editar',$p->id)}}" class="btn btn-secondary">Editar</a>
+                            <a href="#" class="btn btn-danger">Remover</a>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+@endif
+@stop
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/posts','PostsController@lista')->name('posts.listagem')->middleware('auth');
 Route::get('/posts/novo','PostsController@novo')->name('posts.novo');
 Route::post('/posts/adiciona','PostsController@adiciona')->name('posts.adiciona');
+Route::post('/posts/update/{id}','PostsController@update')->name('posts.update');
+Route::get('/posts/editar/{id}','PostsController@editar');
