e49812148fb32d6e84326403f4a79b5b97fa4dda
Anderson Lairton <lairton.anderson@gmail.com>
2020-05-30 16:42:25 -0300

    finalizando formulario,implementando bootstrat

--- resources/views/formulario.blade.php
+++ resources/views/formulario.blade.php
@@ -1,52 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
-    teste
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-
-
-            <div class="card">
-
-                <h1>@if(empty($p))Novo
-                    @else
-                    Editar
-                    @endif
-                    post
-                </h1>
-                <?php $erros = [0]; ?>
-                <!--@if(count($erros)>0)
-                <div class="alert alert-danger">
-    <ul>
-        
-    </ul>
-</div>
-@endif
--->
-                <div class="card-body">
-
-
-                    <form action="{{empty($p)?route('posts.adiciona'):action('PostsController@update',$p->id)}}" method="POST">
-                        @csrf
-                        <div class="form-group">
-                            <label>Titulo</label>
-                            <input type="text" class="form-group" name="titulo" value="{{!empty($p->titulo)?$p->titulo:old('titulo')}}">
-                        </div>
-                        <div class="form-group">
-                            <label>Descricão</label>
-                            <input type="text" class="form-group" name="descricao" value="{{!empty($p->descricao)?$p->descricao:old('descricao')}}">
-                        </div>
-                        <div class="form-group">
-                            <label>Url imagem</label>
-                            <input type="text" class="form-group" name="url_imagem" value="{{!empty($p->url_imagem)?$p->url_imagem:old('url_imagem')}}">
-                        </div>
-                        <button type="submit" class="btn btn-primary btn-block">Salvar</button>
-                    </form>
-                </div>
-            </div>
+    <div class="card">
+        <div class="card-header">
+            @if(empty($post))Novo
+            @else
+            Editar
+            @endif
+            Post
         </div>
+        <div class="card-body">
+            @if(count($errors)>0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+            @endif
 
+            <form action="{{empty($post)?route('posts.adiciona'):action('PostsController@update',$post->id)}}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <div class="form-group">
+                        <label>Titulo</label>
+                        <input type="text" class="form-control" name="titulo" value="{{!empty($post->titulo)?$post->titulo:old('titulo')}}" placeholder="Digite o titulo do post">
+                    </div>
+                    <div class="form-group">
+                        <label>Descricão</label>
+                        <textarea class="form-control" name="descricao" rows="3" placeholder="Digite a descrição do post">{{!empty($post->descricao)?$post->descricao:old('descricao')}}</textarea>
+
+                    </div>
+                    <div class="form-group">
+                        <label>Url imagem</label>
+                        <input type="text" class="form-control" name="url_imagem" value="{{!empty($post->url_imagem)?$post->url_imagem:old('url_imagem')}}" placeholder="Entre com a url da imagem">
+                    </div>
+                </div>
+                <a href="{{route('posts.listagem')}}" class="btn btn-secondary">Voltar</a>
+                <button type="submit" class="btn btn-primary">Salvar</button>
+            </form>
+        </div>
     </div>
-</div>
-@stop
+    @stop
